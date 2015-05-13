c0060048a60ecb1a41e839dc3347ba0583b6291e
Jose V. Trigueros <j.v.trigueros@gmail.com>
2015-05-12 23:38:00 -0600

    Implement authenticate fn.

--- src/ember_grep/core.clj
+++ src/ember_grep/core.clj
@@ -1,11 +1,26 @@
 (ns ember-grep.core
   (:gen-class)
-  (:require [clj-http.client :as client]))
+  (:require [clj-http.client :as client])
+  (:require [clojure.data.json :as json]))
+
+(def ^:private base-url "https://embergrep.com/")
+
+(defn- get-input [prompt]
+  (println prompt)
+  (read-line))
 
 (defn authenticate
-  "Takes ")
+  "Takes a username and password and returns an authentication token"
+  [username password]
+  (let [url (str base-url "/oauth/access_token")
+        form-params {:grant_type "password"
+                     :username username
+                     :password password}
+        response (client/post url {:form-params form-params})
+        body (json/read-str (:body response) :key-fn keyword)]
+    (select-keys body [:access_token :refresh_token])))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (authenticate (get-input "Username: ") (get-input "Password:"))))
